a94a3b30be0b139b50cd4fde15d2544d342a5d4d
ProfessorEngineer.Git <bahrian.novotny@fws-frankfurt.de>
2025-08-06 15:47:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ const joystickZone = document.getElementById('joystick-zone');
 const bottomBar = document.getElementById('bottom-bar');
 const muteButton = document.getElementById('mute-button');
 const analyzeButton = document.getElementById('analyze-button');
+const mapButton = document.getElementById('map-button');
 const audio = document.getElementById('media-player');
 
 // === Hyperspace-Animation Setup ===
@@ -306,4 +307,4 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     composer.setSize(window.innerWidth, window.innerHeight);
     labelRenderer.setSize(window.innerWidth, window.innerHeight);
-});```
+});
